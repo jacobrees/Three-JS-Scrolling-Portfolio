e32ec0e8e7c2e5e5a4bb9002bd1307343c4fd965
jacobrees <jacob.rees97@yahoo.com>
2023-04-09 20:27:49 +0100

    Create basic img slider

---
+++ src/script.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+
+let speed = 0;
+let position = 0;
+let rounded = 0;
+let block = document.querySelector(".block");
+let elems = [...document.querySelectorAll(".n")];
+let wrap = document.querySelector(".wrap");
+
+window.addEventListener("wheel", (e) => {
+  speed += e.deltaY * 0.0003;
+});
+
+let objs = Array(5).fill({ dist: 0 });
+
+function raf() {
+  position += speed;
+  speed *= 0.8;
+
+  objs.forEach((o, i) => {
+    o.dist = Math.min(Math.abs(position - i), 1);
+    o.dist = 1. - o.dist ** 2;
+    elems[i].style.transform = `scale(${1 + 0.4 * o.dist})`;
+  });
+
+  rounded = Math.round(position);
+  let diff = rounded - position;
+
+  position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
+  wrap.style.transform = `translate(0, ${-position * 100 + 50}px)`;
+  window.requestAnimationFrame(raf);
+}
+
+raf();
